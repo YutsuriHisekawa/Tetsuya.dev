5c5d5f13782332b759022ce0993dabb4073397be
YutsuriHisekawa <tetsuyakumiko01@gmail.com>
2023-08-16 15:49:55 +0700

    Portofolio

--- src/script.js
+++ src/script.js
@@ -126,3 +126,37 @@ new ClipboardJS('.code');
          document.getElementById("countdown").innerHTML = "EXPIRED";
      }
  }, 1000);
+
+
+ //typewriter 
+
+const words = ["Hello, World!", "Welcome to my website!", "This is a typewriter effect."];
+let i = 0;
+let j = 0;
+let currentWord = "";
+let isDeleting = false;
+
+function type() {
+  currentWord = words[i];
+  if (isDeleting) {
+    document.getElementById("typewriter").textContent = currentWord.substring(0, j-1);
+    j--;
+    if (j == 0) {
+      isDeleting = false;
+      i++;
+      if (i == words.length) {
+        i = 0;
+      }
+    }
+  } else {
+    document.getElementById("typewriter").textContent = currentWord.substring(0, j+1);
+    j++;
+    if (j == currentWord.length) {
+      isDeleting = true;
+    }
+  }
+  setTimeout(type, 100);
+}
+
+type();
+
